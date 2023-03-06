043c88c8e344906c891cd2d00aaae8ade89cf511
Marcel <marceltrzaskawka129@gmail.com>
2023-03-06 17:10:13 +0100

    example structs completed

---
+++ 5_structs/2_example_structs/src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50
+    };
+
+    println!("The area of a rectangle is {}", area(&rect1));
+
+    dbg!(&rect1);
+
+    println!("rect1 is {:#?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
